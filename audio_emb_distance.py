81c1fb333fb216241cc581bfd4f420dacb65fdb3
Gaofei Shen <g.shen@tilburguniversity.edu>
2022-12-13 15:42:13 +0100

    changed to torchmetrics for cosine similarity calc

--- audio_emb_distance.py
+++ audio_emb_distance.py
@@ -2,9 +2,12 @@ import os
 
 import numpy as np
 import torch
-from sklearn.metrics import pairwise_distances
-import ursa.util as U
 from scipy.spatial.distance import cosine
+from sklearn.metrics import pairwise_distances
+from torchmetrics.functional import pairwise_cosine_similarity
+# device = 'cuda' if torch.cuda.is_available() else 'cpu'
+device = 'cpu'
+import ursa.util as U
 
 
 def audio_pairwise_distance_calc(dataset):
@@ -13,8 +16,12 @@ def audio_pairwise_distance_calc(dataset):
         print('pulling audio embedding from layer', layer)
         audio_emb = [x[0][layer][:-2] for x in dataset]
         # layer_distance = pairwise_distances(audio_emb, metric='cosine', n_jobs=-1)
-        layer_distance = U.pairwise(cosine,audio_emb, parallel=True)
-        pairwise_distance_container[layer] = layer_distance
+        # layer_distance = U.pairwise(cosine,audio_emb, parallel=True)
+        tensor = torch.from_numpy(np.array(audio_emb).astype('float16'))
+        layer_similarity = pairwise_cosine_similarity(tensor.to(device)).cpu()
+        layer_similarity[layer_similarity == 0] = 1
+        # pairwise_distance_container[layer] = layer_distance
+        pairwise_distance_container[layer] = U.triu(layer_similarity)
     return pairwise_distance_container
 
 if __name__ == "__main__":
@@ -26,13 +33,21 @@ if __name__ == "__main__":
     # distance_lookup = {}
 
     for i in dataset_lookup:
-        print('calculating pairwise distance of audio emb from', i)
-        dataset = torch.load(dataset_lookup[i])
-        distance_calculated = audio_pairwise_distance_calc(dataset)
+        save_file = i+'_pairwise_distance.pt'
+        if os.path.isfile(save_file):
+            print(f"{save_file} already exists! skipping for now")
 
-        distance_lookup = distance_calculated
+        else:
+            print('calculating pairwise distance of audio emb from', i)
+            dataset = torch.load(dataset_lookup[i])
+            distance_calculated = audio_pairwise_distance_calc(dataset)
 
-    
-        torch.save(distance_lookup, i+'_pairwise_distance.pt')
+            distance_lookup = distance_calculated
+
+        
+            torch.save(distance_lookup, save_file)
+
+            # break
+
 
 
