00e1c95bbaee7d0baa74bc08b109f68622cc248b
Gaofei Shen <g.shen@tilburguniversity.edu>
2022-12-20 20:12:42 +0100

    adding delex option, adding deberta run results

--- kernel_calc.py
+++ kernel_calc.py
@@ -1,29 +1,36 @@
-import random
 import os
-
-import torch
-
+import random
 
 import numpy as np
+import torch
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 
 import time
 
+from nltk.tree import Tree
 from ursa.kernel import Kernel
 
 tree_paths = {  "scc_trees_path": '/home/gshen/work_dir/spoken-model-syntax-probe/scc_generated_trees.pt', 
                 "libri_trees_path": '/home/gshen/work_dir/spoken-model-syntax-probe/libri_generated_trees.pt'}
 
 
+def delex(n, leaf="X"):
+    if isinstance(n, str): 
+        return leaf 
+    else: 
+        return Tree(n.label(), [ delex(c) for c in n[:] ])
 
 
-def calculate_kernel(tree_list, normalization = True, alpha = 0.5):
+def calculate_kernel(tree_list, normalization = True, alpha = 0.5, delex_option = False):
     K = Kernel(alpha=alpha)
     tree_kernel = []
     for i in range(0, len(tree_list), 2):
-        tree_1, tree_2 = tree_list[i][0][0], tree_list[i+1][0][0]
+        if delex_option:
+            tree_1, tree_2 = tree_list[i][0][0], tree_list[i+1][0][0]
+        else:
+            tree_1, tree_2 = delex(tree_list[i][0][0]), delex(tree_list[i+1][0][0])
         kern = K(tree_1, tree_2)
         if normalization == True:
             # Normalize the kernels calculated 
@@ -38,10 +45,13 @@ def calculate_kernel(tree_list, normalization = True, alpha = 0.5):
     return tree_kernel
 
 
-def main(seed = 42, alpha = 0.5):
+def main(seed = 42, alpha = 0.5, delex_option = False):
     random.seed(seed)
     for select_ in tree_paths:
-        save_file = os.path.join('tree_kernel', select_[:-4]+str(alpha)+'_'+str(seed)+'_'+'kernel.pt')
+        if delex_option:
+            save_file = os.path.join('tree_kernel', select_[:-4]+str(alpha)+'_'+str(seed)+'_'+'delexed'+'_'+'kernel.pt')
+        else:
+            save_file = os.path.join('tree_kernel', select_[:-4]+str(alpha)+'_'+str(seed)+'_'+'kernel.pt')
 
         if os.path.isfile(save_file):
             print(save_file + ' already exists! skipping')
@@ -54,8 +64,6 @@ def main(seed = 42, alpha = 0.5):
                 trees_filtered = [x for x in tree_list if len(str.split(x[1])) < 52]
             trees_filtered = [[item, i] for i, item in enumerate(trees_filtered)]
             st = time.time()
-            # df = calculate_kernel(tree_list)
-            # df.to_csv(select_[:-4]+'kernel.csv')
             random.shuffle(trees_filtered)
             tree_kernel = calculate_kernel(trees_filtered, alpha = alpha)
             torch.save(tree_kernel, save_file)
@@ -63,6 +71,13 @@ def main(seed = 42, alpha = 0.5):
             print('script ran for', et-st, 'seconds for', select_[:-5])
 
 if __name__ == "__main__":
+    main(seed = 42)
     main(seed = 666)    
+    main(seed = 19)
+    main(seed = 2022)
+    main(seed = 42, delex_option=True)
+    main(seed = 666, delex_option=True)   
+    main(seed = 19, delex_option=True)
+    main(seed = 2022, delex_option=True)
 
 
