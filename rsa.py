1798c9e19da469f598e8d527d09030e664031d35
Gaofei Shen <g.shen@tilburguniversity.edu>
2023-01-06 15:41:11 +0100

    separated rsa and rsa_regress

--- rsa.py
+++ rsa.py
@@ -1,54 +1,16 @@
 import numpy as np
 import torch
-
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
-
-
 import os
-
 from scipy.stats import pearsonr
-
-from sklearn.linear_model import Ridge
-from sklearn.model_selection import GridSearchCV
-from sklearn.metrics.pairwise import cosine_similarity
 import ursa.util as U
-from sklearn.metrics import mean_squared_error, r2_score, make_scorer
+
+
 
 def pearson_r_score(Y_true, Y_pred): 
      r =  U.pearson_r(Y_true, Y_pred, axis=0).mean() 
      return r
 
-class Regress:
-
-    default_alphas = [ 10**n for n in range(-3, 2) ]
-    metrics = dict(mse       = make_scorer(mean_squared_error, greater_is_better=False),
-                   r2        = make_scorer(r2_score, greater_is_better=True),
-                   pearson_r = make_scorer(pearson_r_score, greater_is_better=True))
-                   
-    
-    def __init__(self, cv=10, alphas=default_alphas):
-        self.cv = cv
-        self.grid =  {'alpha': alphas }
-        self._model = GridSearchCV(Ridge(), self.grid, scoring=self.metrics, cv=self.cv, return_train_score=False, refit=False)
-
-    def fit(self, X, Y):
-        self._model.fit(X, Y)
-        result = { name: {} for name in self.metrics.keys() }
-        for name, scorer in self.metrics.items():
-            mean = self._model.cv_results_["mean_test_{}".format(name)] 
-            std  = self._model.cv_results_["std_test_{}".format(name)]
-            best = mean.argmax()
-            result[name]['mean'] = mean[best] * scorer._sign
-            result[name]['std']  = std[best]
-            result[name]['alpha'] = self.grid['alpha'][best]
-        self._report = result
-
-    def fit_report(self, X, Y):
-        self.fit(X, Y)
-        return self.report()
-
-    def report(self):
-        return self._report
 
 tree_kernel_path = '/home/gshen/work_dir/spoken-model-syntax-probe/tree_kernel'
 kernel_files = [os.path.join(tree_kernel_path,x) for x in os.listdir(tree_kernel_path) if '_kernel' in x]
@@ -58,7 +20,7 @@ pairwise_distance_path = '/home/gshen/work_dir/spoken-model-syntax-probe/pairwis
 
 pairwise_distance_files = [os.path.join(pairwise_distance_path,x) for x in os.listdir(pairwise_distance_path) if 'full' in x]
 
-def main(alpha = 0.5, seed = 42, mode = 'rsa', delexed = True):
+def main(alpha = 0.5, seed = 42, delexed = True):
     alpha = str(alpha)
     seed = str(seed)
     for x in pairwise_distance_files:
@@ -88,14 +50,9 @@ def main(alpha = 0.5, seed = 42, mode = 'rsa', delexed = True):
         for layer in range(len(calculated_distances[:-1])):
             # audio_distances = [calculated_distances[layer][x[0],x[1]] for x in kernel_pairs]
             audio_distances = np.array([calculated_distances[layer][x[0],x[1]] for x in kernel_pairs])
-            if mode == 'rsa':
-                r_score = pearsonr(kernel, audio_distances)
-                print((modelname, datasetname, layer, alpha) + r_score)
+            r_score = pearsonr(kernel, audio_distances)
+            print((modelname, datasetname, layer, alpha) + r_score)
 
-            elif mode == 'rsa-regress':
-                R = Regress()
-                score = R.fit_report(X =  kernel.reshape(-1, 1), Y = audio_distances)
-                print((modelname, datasetname, layer), score)
 
 def plot(alpha = 0.5, seed = 42, mode = 'rsa'):
     import numpy as np
@@ -138,8 +95,3 @@ if __name__ == "__main__":
     main(seed = 42, delexed = True)
     main(seed = 666, delexed = True)
     main(seed = 2022, delexed = True)
-
-    # main(seed = 42, mode = 'rsa-regress')
-    # main(seed = 666, mode = 'rsa-regress')
-    # main(seed = 2022, mode = 'rsa-regress')
-    # plot()
